c812c9578b40d54bcd515437f5ddafd894d43843
Niels Maes <maes.niels@telenet.be>
2024-12-06 16:59:24 +0100

    made animations to the navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,33 +6,31 @@
         <router-link to="/" class="text-white text-xl font-bold">Portfolio Niels</router-link>
       </div>
       <!-- Centered Links -->
-      <div class="absolute left-1/2 transform -translate-x-1/2 mt-4">
-        <div class="border-4 border-purple-accent rounded-full py-2 px-6 flex flex-wrap justify-center space-x-6">
-          <router-link
-              to="/"
-              class="hover:text-purple-accent transition duration-300"
-          >
-            Home
-          </router-link>
-          <router-link
-              to="/about"
-              class="hover:text-purple-accent transition duration-300"
-          >
-            About
-          </router-link>
-          <router-link
-              to="/projects"
-              class="hover:text-purple-accent transition duration-300"
-          >
-            Projects
-          </router-link>
-          <router-link
-              to="/contact"
-              class="hover:text-purple-accent transition duration-300"
-          >
-            Contact
-          </router-link>
-        </div>
+      <div class="absolute left-1/2 transform -translate-x-1/2 mt-4 flex space-x-6">
+        <router-link
+            to="/"
+            class="nav-link hover:text-purple-accent transition duration-300"
+        >
+          Home
+        </router-link>
+        <router-link
+            to="/about"
+            class="nav-link hover:text-purple-accent transition duration-300"
+        >
+          About
+        </router-link>
+        <router-link
+            to="/projects"
+            class="nav-link hover:text-purple-accent transition duration-300"
+        >
+          Projects
+        </router-link>
+        <router-link
+            to="/contact"
+            class="nav-link hover:text-purple-accent transition duration-300"
+        >
+          Contact
+        </router-link>
       </div>
     </div>
   </nav>
@@ -52,3 +50,24 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.nav-link {
+  position: relative;
+}
+
+.nav-link::after {
+  content: '';
+  position: absolute;
+  width: 0;
+  height: 4px;
+  bottom: -4px;
+  left: 0;
+  background-color: #6a0dad;
+  transition: width 0.3s ease-in-out;
+}
+
+.nav-link:hover::after {
+  width: 100%;
+}
+</style>
