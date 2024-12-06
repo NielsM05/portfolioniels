8d805af7ea3616a5edd3cb8f429cf8f87f2fa5a1
Niels Maes <maes.niels@telenet.be>
2024-12-06 16:40:54 +0100

    Basic start page

---
+++ src/components/Navbar.vue
@@ -0,0 +1,54 @@
+<template>
+  <nav class="bg-dark-blue text-white fixed w-full z-10">
+    <div class="container mx-auto px-4 flex items-center h-16 relative">
+      <!-- Site Name -->
+      <div class="logo">
+        <router-link to="/" class="text-white text-xl font-bold">Portfolio Niels</router-link>
+      </div>
+      <!-- Centered Links -->
+      <div class="absolute left-1/2 transform -translate-x-1/2">
+        <div class="flex space-x-6">
+          <router-link
+              to="/"
+              class="hover:text-blue-accent transition duration-300"
+          >
+            Home
+          </router-link>
+          <router-link
+              to="/about"
+              class="hover:text-blue-accent transition duration-300"
+          >
+            About
+          </router-link>
+          <router-link
+              to="/projects"
+              class="hover:text-blue-accent transition duration-300"
+          >
+            Projects
+          </router-link>
+          <router-link
+              to="/contact"
+              class="hover:text-blue-accent transition duration-300"
+          >
+            Contact
+          </router-link>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isMenuOpen: false,
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.isMenuOpen = !this.isMenuOpen;
+    },
+  },
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,21 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+  <div class="bg-dark-blue text-white min-h-screen flex items-center justify-center">
+    <div class="text-center">
+      <h1 class="text-4xl font-bold mb-4">Welcome to My Portfolio</h1>
+      <p class="text-lg mb-8">Explore my projects and learn more about me.</p>
+      <router-link to="/projects" class="bg-blue-accent text-white py-2 px-4 rounded hover:bg-blue-700 transition duration-300">
+        More about me
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+};
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped>
+/* Add any additional styles here */
+</style>
