bb59ac31fe74bb4464b3d4a5e566c30f45a73437
Niels Maes <maes.niels@telenet.be>
2024-12-06 16:52:51 +0100

    added round around the navbar links

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,12 +2,12 @@
   <nav class="bg-darkest-blue text-white fixed w-full z-10">
     <div class="container mx-auto px-4 flex items-center h-16 relative">
       <!-- Site Name -->
-      <div class="logo">
+      <div class="logo border-b-4 border-purple-accent">
         <router-link to="/" class="text-white text-xl font-bold">Portfolio Niels</router-link>
       </div>
       <!-- Centered Links -->
-      <div class="absolute left-1/2 transform -translate-x-1/2">
-        <div class="flex space-x-6">
+      <div class="absolute left-1/2 transform -translate-x-1/2 mt-4">
+        <div class="border-4 border-purple-accent rounded-full py-2 px-6 flex flex-wrap justify-center space-x-6">
           <router-link
               to="/"
               class="hover:text-purple-accent transition duration-300"
